597cffca4b5ea2b5523f5be55a70be00975717d2
Landerson <clandokca@gmail.com>
2020-06-23 20:44:18 -0300

    Início do versionamento.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using CrudAPI.Models;
+
+namespace CrudAPI.Data {
+  public class DataContext : DbContext {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+  }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudAPI.Models {
+  public class Category {
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [MaxLength(60, ErrorMessage = "Este campo deve ter entre 3 e 60 caracteres")]
+    [MinLength(3, ErrorMessage = "Este campo deve ter entre 3 e 60 caracteres")]
+    public string Title { get; set; }
+  }
+}
